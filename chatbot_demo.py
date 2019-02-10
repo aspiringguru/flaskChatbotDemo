2a8766768c133c9c738097a32527e2c975fa1b1f
aspiringguru <bmatthewtaylor@gmail.com>
2019-02-10 13:12:29 +1000

    wip

--- chatbot_demo.py
+++ chatbot_demo.py
@@ -1,10 +1,27 @@
-from flask import Flask
+import gevent.monkey
+gevent.monkey.patch_all()
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
+
+@app.route('/')
+def sessions():
+    return render_template('session.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+    print('This is error output', file=sys.stderr)
+    print('This is standard output', file=sys.stdout)
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=80)
